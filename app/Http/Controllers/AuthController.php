d6ead6b22619e1746b204dbee94334887289f436
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-03-14 23:19:10 +0100

    create login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,4 +36,25 @@ class AuthController extends Controller
         ->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer', ]);
 
    }
+
+   public function login(Request $request)
+   {
+    if (!Auth::attempt($request->only('email', 'password')))
+    {
+        return response()
+        ->json(['message' => 'Unauthorized', 401]);
+    }
+
+    $user = User::where('email', $request['email'])->firstOrFail();
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()
+        ->json([
+            'message' => 'Hi ',$user->name,
+            'accessToken' => $token,
+            'token_type' => 'Bearer',
+            'user'=>$user,
+        ]);
+   }
 }
